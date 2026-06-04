fb41a5c111d3686b3858a99d35898e49c6aa137c
codingquokka <104996819+l1lsang@users.noreply.github.com>
2026-06-04 14:45:24 +0900

    [Passed] 반복 출력하기 (2ms, 1MB)

---
+++ trail2/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void printx(int x){
+    for(int i=0;i<x;i++){
+        cout<<"12345^&*()_"<<endl;
+    }
+    return;
+}
+int main() {
+    int x;
+    cin >>x;
+    printx(x);
+    return 0;
+}
